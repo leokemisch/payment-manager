b630ab50ca2da9dda0493892f6bddea71e856564
Leonardo Kemisch <leonardokemisch@gmail.com>
2025-03-01 11:56:18 -0300

    fist commit: project structure

---
+++ src/main/java/com/github/payment_manager/PaymentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.payment_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentManagerApplication.class, args);
+	}
+
+}
